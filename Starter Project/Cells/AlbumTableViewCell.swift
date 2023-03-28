0c3178b84036facc2de3f829f2e8e2dc3c8d2f61
A-Fawzyy <fawzy3171@gmail.com>
2023-03-28 23:31:56 +0200

    feat(AlbumTableView): Add initial implementation
    Implement Album table view with just printing from mocks

---
+++ Starter Project/Cells/AlbumTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AlbumTableViewCell.swift
+//  Starter Project
+//
+//  Created by Abdelhamid Nasser on 28/03/2023.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configure(item: AlbumModel) {
+        let imageUrl = item.urls.regular
+        guard let url = URL(string: imageUrl) else {
+            return
+        }
+        debugPrint("url: \(url)")
+    }
+    
+}
--- Starter Project/ViewController.swift
+++ Starter Project/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet private weak var albumsTableView: UITableView!
+    
+    var albumsList: [AlbumModel] = []
+    
+    let albumCellId = "AlbumTableViewCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        retrievePictures()
+        albumsTableView.register(UINib(nibName: albumCellId, bundle: nil), forCellReuseIdentifier: albumCellId)
+        albumsTableView.delegate = self
+        albumsTableView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        albumsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = albumsTableView.dequeueReusableCell(withIdentifier: albumCellId, for: indexPath) as! AlbumTableViewCell
+        
+        let album = albumsList[indexPath.row]
+        
+        cell.configure(item: album)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        debugPrint("clicked Row: \(indexPath.row)")
+    }
+    
+    func retrievePictures() {
+        self.albumsList = DataMock.dummyAlbums
+        debugPrint("loaded pictures succcessfully from mock")
+    }
 }
-
